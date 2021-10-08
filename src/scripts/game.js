b011814c7a4b39928ef7addcbb30c8cd3147af1e
Subin Cho <subinee1633@gmail.com>
2021-10-08 15:47:16 -0400

    added more functions to game

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,14 +1,28 @@
-import Avatar from "avatar.js";
-import Arrow from "arrow.js";
+import Avatar from "./avatar.js";
+import Arrow from "./arrow.js";
 
 class Game {
     constructor() {
         this.arrow = [];
         this.avatar = [];
+        this.X_DIM = 900;
+        this.Y_DIM = 630;
+        this.BG_COLOR = ("#bf75be");
     }
 
+    draw(ctx) {
+        ctx.fillStyle = this.BG_COLOR;
+        ctx.fill();
+    }
 
+    addArrow() {
+        const leftArrow = new Arrow();
+        const upArrow = new Arrow();
+        const downArrow = new Arrow();
+        const rightArrow = new Arrow();
+    }
+
+    randomArrow() {
+        let randomArrow = this.arrow[Math.floor(Math.random() * this.arrow.length)];
+    }
 }
-
-Game.X_DIM = 900;
-Game.Y_DIM = 630;
