bce2409d779b7f027fbb87db7fffcab3a695ad79
Subin Cho <subinee1633@gmail.com>
2021-10-09 00:49:47 -0400

    created newArrow function

--- src/scripts/arrow.js
+++ src/scripts/arrow.js
@@ -6,24 +6,9 @@ const PROPERTIES = {
 export default class Arrow { 
     constructor() {
         this.color = "#ffffff";
-        this.strokeColor = `rgb(
-            ${Math.floor(240 - 30 * i)},
-            0,
-            ${Math.floor(240 - 30 * j)})`
+        this.strokeColor = "#ed1851"
         this.size = PROPERTIES.SIZE;
         this.speed = PROPERTIES.SPEED;
-    }
-
-    draw(ctx) {
-        ctx.fillStyle = this.color;
-        this.leftArrow = ctx.fillRect(300, 315, 100, 100);
-
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                ctx.strokeStyle = this.strokeColor;
-                ctx.strokeRect(300, 315, 100, 100);
-            }
-        }
     }
 
     newArrow(ctx, startX, startY, endX, endY) {
@@ -33,8 +18,4 @@ export default class Arrow {
         ctx.stroke();
         ctx.closePath();
     }
-
-
-
-    
 }
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -8,10 +8,6 @@ export default class Game {
         this.BG_COLOR = "#e6c7f0";
         this.DIM_X = 900;
         this.DIM_Y = 630;
-        // this.leftArrow = leftArrow;
-        // this.rightArrow = rightArrow;
-        // this.upArrow = upArrow;
-        // this.downArrow = downArrow;
     }
 
     draw(ctx) {
@@ -19,12 +15,13 @@ export default class Game {
         ctx.fillRect(0, 0, this.DIM_X, this.DIM_Y);
     }
 
-    createArrow(ctx) {
-        const newArrow = new Arrow();
+    createArrow() {
+        let newArrow = new Arrow();
+        this.arrow.push(newArrow);
     }
 
     randomArrow() {
-        let randomArrow = this.arrow[Math.floor(Math.random() * this.arrow.length)];
+        let rand = this.arrow[Math.floor(Math.random() * this.arrow.length)];
     }
 
     drawScore() {
