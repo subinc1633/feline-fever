14f18d0d9f3971946213245b6275945fda775830
Subin Cho <78716708+subinc1633@users.noreply.github.com>
2021-12-16 19:01:39 -0500

    remove comments

--- src/scripts/arrow.js
+++ src/scripts/arrow.js
@@ -29,21 +29,6 @@ export default class Arrow {
             default:
                 break;
         }
-
-
-        // if (this.dir === 'left') {
-        //     const leftArrow = document.getElementById("leftArrow");
-        //     ctx.drawImage(leftArrow, this.x, this.y);
-        // } else if (this.dir === "up") {
-        //     const upArrow = document.getElementById("upArrow");
-        //     ctx.drawImage(upArrow, this.x, this.y);
-        // } else if (this.dir === "down") {
-        //     const downArrow = document.getElementById("downArrow");
-        //     ctx.drawImage(downArrow, this.x, this.y);
-        // } else if (this.dir === "right") {
-        //     const rightArrow = document.getElementById("rightArrow");
-        //     ctx.drawImage(rightArrow, this.x, this.y);
-        // }
     }
 
     move() {
@@ -52,8 +37,6 @@ export default class Arrow {
 
         if (leftRight && this.y < 530 || upDown && this.y < 530) {
             this.y += this.speed * this.dt;
-            
-            // this.y += this.speed;
         } else {
             this.deletion = true;
         }
